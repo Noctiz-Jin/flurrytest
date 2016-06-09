a2d427a31f22fa7bef3131a95c5df7ec50da4f79
Noctiz <noctizgame@gmail.com>
2016-06-08 23:37:29 -0700

    Initial Commit

---
+++ flurry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flurry
+//
+//  Created by Yuning Jin on 6/8/16.
+//  Copyright © 2016 Noctiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ flurry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  flurry
+//
+//  Created by Yuning Jin on 6/8/16.
+//  Copyright © 2016 Noctiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
